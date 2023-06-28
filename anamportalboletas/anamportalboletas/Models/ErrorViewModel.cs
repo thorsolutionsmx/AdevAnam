adc9b9eb3396137348e064f6ea8e1537c701a4df
José Francisco Díaz Castillo <jodiazc@microsoft.com>
2023-06-28 09:40:56 -0600

    Cambios en el código se aumenta la página de boletas y se agregan nugets

---
+++ anamportalboletas/anamportalboletas/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace anamportalboletas.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
