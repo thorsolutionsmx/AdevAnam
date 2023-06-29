31bf2981d1a638086d88d5dde3b6defdd2261b4e
Daniel <mario.ramirez@anam.gob.mx>
2023-06-29 17:40:19 -0600

    Tarea realizada AB#434

--- anamportalboletas/anamportalboletas/Controllers/HomeController.cs
+++ anamportalboletas/anamportalboletas/Controllers/HomeController.cs
@@ -28,6 +28,11 @@ namespace anamportalboletas.Controllers
             return View();
         }
 
+        public IActionResult Formulario()
+        {
+            return View();
+        }
+
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
